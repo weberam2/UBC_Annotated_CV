f6f669741b4891a2ae0c01b99782dd671e34a913
weberam2 <weberam2@gmail.com>
2025-07-25 13:48:24 -0700

    changed from poetry to uv

--- createpdf.sh
+++ createpdf.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+echo "Running boldauthors.sh"
 ./boldauthors.sh
 
-poetry run python3 cite.py
+echo "Running cite.py"
+uv run python3 cite.py
 #
-poetry run python3 altcite.py
+echo "Running altcite.py"
+uv run python3 altcite.py
 #
-poetry run python3 githubstats.py
-#
+echo "Running githubstats.py"
+uv run python3 githubstats.py
+
+echo "Running pdflatex and bibtex"
 pdflatex UBC_CV
 bibtex UBC_CV
 pdflatex UBC_CV
